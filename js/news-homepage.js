69e74e69dd00b3bfac97a9413b82096b8e3ea472
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-05-07 06:30:18 -0700

    Added JS file to the project directory

---
+++ js/news-homepage.js
@@ -0,0 +1,22 @@
+const menuIcon = document.querySelector("img[alt='menu-open icon']");
+const closeIcon = document.querySelector("img[alt='menu-close icon']");
+const mobileMenu = document.querySelector(".mobile-menu");
+const bodyElement = document.querySelector("body");
+
+// This function opens the mobile side-menu.
+const openMenu = () => {
+    mobileMenu.style.display = "flex";
+    bodyElement.style.backgroundColor = "hsl(233, 8%, 79%)";
+};
+
+// This function closes the mobile side-menu.
+const closeMenu = () => {
+    mobileMenu.style.display = "none";
+    bodyElement.style.backgroundColor = "hsl(36, 100%, 99%)";
+};
+
+/* Event Listeners */
+
+menuIcon.addEventListener("click", openMenu);
+
+closeIcon.addEventListener("click", closeMenu);
